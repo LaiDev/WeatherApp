8e457c226f328756e9772e5bc021038faa5654ed
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-12 13:48:47 -0400

    Add API to get Weather Information from Zipcode

--- src/app.js
+++ src/app.js
@@ -1,66 +1,28 @@
-let zipCode = document.querySelector("#zipCode");
+//Uses the zipcode to return an object containing information about the users location. This includes : City Name, Country, Latitude, and Longitude
 
-//Gets information about the users location from the inputted zipcode
-
-export async function getLocationData() {
-  let locationResponse = await fetch(
-    `http://api.openweathermap.org/geo/1.0/zip?zip=${30241}&appid=674d4fcffa55cdf3f103af8e346807c8`,
+async function getLocationData(zipCode) {
+  //assign the fetched data to locationData once it gets it
+  const locationData = await fetch(
+    `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=674d4fcffa55cdf3f103af8e346807c8`,
     { mode: `cors` }
   );
 
-  let locationData = await locationResponse.json();
+  const locationObj = await locationData.json();
 
-  return locationData;
+  console.log(locationObj);
+
+  //Calls getWeatherData() and inputs the lat and lon
+  getWeatherData(locationObj.lat, locationObj.lon);
 }
 
-//Gets the location lat and lon from the zipcode
-
-/*
-async function getLocationData() {
-  let locationResponse = await fetch(
-    `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode.value}&appid=674d4fcffa55cdf3f103af8e346807c8`,
-    { mode: `cors` }
+async function getWeatherData(lat, lon) {
+  const weatherData = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=674d4fcffa55cdf3f103af8e346807c8`
   );
 
-  let locationData = await locationResponse.json();
-
-  console.log(locationData);
+  const weatherObj = await weatherData.json();
+  
+  console.log(weatherObj);
 }
-*/
 
-/*
-  console.log(locationData);
-
-  //Uses the lat and lon from locationResponse to get the actual weather data
-
-  let weatherResposne = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&appid=674d4fcffa55cdf3f103af8e346807c8`,
-    { mode: `cors` }
-  );
-  let weatherData = await weatherResposne.json();
-
-  console.log(weatherData);
-
-  const name = locationData.name;
-  //Converts the temp from Kelvin to Fahrenheit
-  const temp = Math.ceil((weatherData.main.temp - 273.15) * 9/5 + 32);
-
-*/
-/*
-  const city = document.querySelector(".city");
-  city.innerText = locationData.name;
-
-  let weatherType = document.querySelector(".weatherType");
-  let currentWeather = weatherData.weather[0].main;
-
-  switch (currentWeather) {
-    case "Clouds":
-      console.log("Cloudy");
-      weatherType.innerText = "☁️";
-      break;
-    case "Rain":
-      console.log("Raining");
-      weatherType.innerText = "🌧️";
-      break;
-  }
-  */
+getLocationData(30241);
