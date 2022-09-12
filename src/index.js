b65432cdf59d0536beb41488ac3be62e6d791513
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-12 14:20:19 -0400

    Add event when inputing zipcode

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { getLocationData } from "./app";
+import { updateDisplay } from "./dom";
+
+let userZipCode = document.querySelector("#zipCode");
+const searchBtn = document.querySelector("#searchBtn");
+
+searchBtn.addEventListener("click", handleThis);
+
+//When the button is clicked, called getlocationData to get the data for the inputed zipcode. Using that information, call a function which will update the ui with the proper values
+
+function handleThis() {
+
+  let location = getLocationData(userZipCode.value);
+
+  updateDisplay(location)
+}
