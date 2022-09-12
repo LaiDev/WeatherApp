c0629e43989773f58875d528059f91e1bae1cfe5
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-12 15:07:35 -0400

    Update Temp UI based on location

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,20 @@
 let cityName = document.querySelector(".city");
 let temp = document.querySelector(".temp");
 
-function updateDisplay(location) {
-
+function updateDisplay(location, weather) {
   //Gets the data from the argument and stores it as a variable called response
-  location.then(function (response) {
-    cityName.innerText = response.name;
+  location.then(function (locationData) {
+    cityName.innerText = locationData.name;
+  });
+
+  weather.then(function (weatherData) {
+    console.log(weatherData, "Success!");
+
+    let fahrenheitTemp = Math.round(
+      ((weatherData.main.temp - 273.15) * 9) / 5 + 32
+    );
+
+    temp.innerText = fahrenheitTemp;
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getLocationData } from "./app";
+import { getLocationData, getWeatherData } from "./app";
 import { updateDisplay } from "./dom";
 
 let userZipCode = document.querySelector("#zipCode");
@@ -9,8 +9,11 @@ searchBtn.addEventListener("click", handleThis);
 //When the button is clicked, called getlocationData to get the data for the inputed zipcode. Using that information, call a function which will update the ui with the proper values
 
 function handleThis() {
-
   let location = getLocationData(userZipCode.value);
 
-  updateDisplay(location)
+  location.then(function (response) {
+    let weather = getWeatherData(response.lat, response.lon);
+
+    updateDisplay(location, weather);
+  });
 }
