3e5af9a0901cde7bee21259a94cdc2e873e469bd
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-12 13:49:24 -0400

    remove unnecessary code

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { getLocationData } from "./app";
-import { changeLocationText } from "./dom";
-
-const searchButton = document.querySelector("#searchBtn");
-
-searchButton.addEventListener("click", changeLocationText);
