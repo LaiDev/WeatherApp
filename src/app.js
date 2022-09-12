e4d5a86b6dd8f02473da7b163e96632a554dd4e3
Aalaizha <l.bnjamin0309@gmail.com>
2022-09-12 14:55:31 -0400

    Get weather and location from API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 //Uses the zipcode to return an object containing information about the users location. This includes : City Name, Country, Latitude, and Longitude
 
 export async function getLocationData(zipCode) {
-  //assign the fetched data to locationData once it gets it
   const locationData = await fetch(
     `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=674d4fcffa55cdf3f103af8e346807c8`,
     { mode: `cors` }
@@ -9,15 +8,10 @@ export async function getLocationData(zipCode) {
 
   const locationObj = await locationData.json();
 
-  console.log(locationObj);
-  
-  getWeatherData(locationObj.lat, locationObj.lon);
-  
   return locationObj;
-  //Calls getWeatherData() and inputs the lat and lon
 }
 
-async function getWeatherData(lat, lon) {
+export async function getWeatherData(lat, lon) {
   const weatherData = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=674d4fcffa55cdf3f103af8e346807c8`
   );
@@ -25,4 +19,6 @@ async function getWeatherData(lat, lon) {
   const weatherObj = await weatherData.json();
 
   console.log(weatherObj);
+
+  return weatherObj;
 }
